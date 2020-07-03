9597c6117033e3d3da651bebc9f8cc1c64380c49
Anders Heilemann <a.heilemann@gmail.com>
2020-07-03 12:26:55 +0200

    Review model and migration

--- reviewsite/reviews/models.py
+++ reviewsite/reviews/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
+from model_utils.models import TimeStampedModel
+
+from autoslug import AutoSlugField
 
 # Create your models here.
+class Review(TimeStampedModel):
+    title = models.CharField("Review title", max_length=255)
+    description = models.TextField("Review", blank=True)
+    slug = AutoSlugField("Review Address",
+                         unique=True,
+                         always_update=False,
+                         populate_from="title")
+                         
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.CASCADE,
+        # limit_choices_to={'is_author': True},
+    )
+
+    def __str__(self):
+        return self.title
