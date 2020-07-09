98b14bd8a4d493cfeb003309e374d4b20126a41b
Anders Heilemann <a.heilemann@gmail.com>
2020-07-09 13:57:40 +0200

    Viewing categories now supported in reviewList

--- reviewsite/reviews/urls.py
+++ reviewsite/reviews/urls.py
@@ -15,11 +15,6 @@ urlpatterns = [
         name='add'
     ),
     path(
-        route='categories/',
-        view=views.CategoryListView.as_view(),
-        name='categoryList'
-    ),
-    path(
         route='<slug:slug>/',
         view=views.ReviewDetailView.as_view(),
         name='detail'
