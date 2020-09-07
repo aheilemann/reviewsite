0ee778d2da6cc2d326f55d6ad34bf4fed75eb760
Anders Heilemann <a.heilemann@gmail.com>
2020-09-07 14:45:19 +0200

    Refactor

--- reviewsite/reviews/urls.py
+++ reviewsite/reviews/urls.py
@@ -3,26 +3,14 @@ from django.urls import path
 from . import views
 
 app_name = "reviews"
-urlpatterns = [
-    path(
-        route='',
-        view=views.ReviewListView.as_view(),
-        name='list'
-    ),
-    path(
-        route='add/',
-        view=views.ReviewCreateView.as_view(),
-        name='add'
-    ),
-    path(
-        route='<slug:slug>/',
-        view=views.ReviewDetailView.as_view(),
-        name='detail'
-    ),
-    path(
-        route='<slug:slug>/update/',
-        view=views.ReviewUpdateView.as_view(),
-        name='update'
-    ),
 
+urlpatterns = [
+    path(route="", view=views.ReviewListView.as_view(), name="list"),
+    path(route="add/", view=views.ReviewCreateView.as_view(), name="add"),
+    path(route="<slug:slug>/", view=views.ReviewDetailView.as_view(), name="detail"),
+    path(
+        route="<slug:slug>/update/",
+        view=views.ReviewUpdateView.as_view(),
+        name="update",
+    ),
 ]
