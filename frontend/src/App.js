50eda8e7d8e12a93a6e0050c5efc2e0719cead37
Anders Heilemann <a.heilemann@gmail.com>
2020-09-09 17:38:03 +0200

    Moved to class based react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  function handleClick(e) {
-    e.preventDefault();
-    console.log('The link was clicked.');
+class App extends Component {
+  state = {
+    reviews: [],
+    currentuser: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('/api/reviews');
+      const reviews = await res.json();
+      this.setState({
+        reviews
+      });
+    } catch (e) {
+      console.log(e);
+    }
+    try {
+      const res = await fetch('/api/currentuser');
+      const currentuser = await res.json();
+      this.setState({
+        currentuser
+      });
+    } catch (e) {
+      console.log(e);
+    }
   }
 
-  // store reviews in a new variable
-  const [reviews, setReviews] = useState([]);
-
-  useEffect(() => {
-    let fetchData = async () => {
-      let response = await fetch('/api/reviews')
-      let json = await response.json()
-      setReviews(json)
-    }
-    fetchData()
-  }, []);
-
-  const [user, setUser] = useState([]);
-
-  useEffect(() => {
-    let fetchData = async () => {
-      let response = await fetch('/api/users/current')
-      let json = await response.json()
-      setUser(json)
-    }
-    fetchData()
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Reviews</h1>
-        {reviews.map((review) => <p>
-          <button onClick={handleClick}>VoteUP</button>
-          {user.username}
-          <a href={review.url}>{review.title}</a> (by: <a href={review.author_url}>{review.author}</a> - Votes: {review.vote_score}) <br></br> {review.description}
-        </p>
-        )}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        {this.state.reviews.map(review => (
+          <div key={review.id}>
+            <h1><a href={review.url}>{review.title}</a></h1>
+            
+            by: <a href={review.author_url}>{review.author}(id: {this.state.currentuser.id})</a> - Votes: {review.vote_score} <p></p> {review.description}
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
