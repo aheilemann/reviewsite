9792f7a8d8d16683bf49bc0d01b30ac86cbead22
Anders Heilemann <a.heilemann@gmail.com>
2020-09-11 16:28:46 +0200

    Frontend voting buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component  } from 'react';
+// import './App.css';
 
 class App extends Component {
   state = {
     reviews: [],
     currentuser: [],
-  };
+  }
 
   async componentDidMount() {
-    try {
-      const res = await fetch('/api/reviews');
-      const reviews = await res.json();
-      this.setState({
-        reviews
-      });
-    } catch (e) {
-      console.log(e);
-    }
-    try {
-      const res = await fetch('/api/currentuser');
-      const currentuser = await res.json();
-      this.setState({
-        currentuser
-      });
-    } catch (e) {
-      console.log(e);
-    }
+    Promise.all([
+      fetch('/api/reviews'),
+      // fetch('/api/currentuser'),
+      fetch('/api/users/current/')
+    ])
+    .then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
+    .then(([data1, data2]) => this.setState({
+      reviews: data1, 
+      currentuser: data2
+    }));
+}
+
+  handleClick = (review, vote='up') => {
+    fetch('http://127.0.0.1:8000/api/reviews/'+ review.id +'/vote/', {
+      method: 'POST',
+      body: JSON.stringify({
+        // user_id: this.currentuserid,
+        'action' : this.vote,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        'Authorization': 'Basic ' + btoa('reviewuser:Hoxdo1-wetryt-ruqxuj'),
+      }
+    })
+    // .then(response => response.json())
+    // .then(json => console.log(json))
   }
 
   render() {
     return (
-      <div>
-        {this.state.reviews.map(review => (
-          <div key={review.id}>
-            <h1><a href={review.url}>{review.title}</a></h1>
-            
-            by: <a href={review.author_url}>{review.author}(id: {this.state.currentuser.id})</a> - Votes: {review.vote_score} <p></p> {review.description}
-          </div>
-        ))}
+      <div className="App">
+        <header className="App-header">
+          {this.state.reviews.map(review => (
+            <div key={review.id}>
+              <h1><a href={review.url}>{review.title}</a></h1>
+              by: <a href={review.author_url}>{review.author}</a> - 
+              Votes: {review.vote_score} 
+              <button onClick={() => this.handleClick(review, this.vote='up')}>Upvote!</button>
+              <button onClick={() => this.handleClick(review, this.vote='down')}>Downvote!</button>
+              
+              <p />{review.description}
+            </div>
+          ))}
+        </header>
       </div>
     );
   }
