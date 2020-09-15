228993b5f926cb9f4379656207265bcd9abb8876
Anders Heilemann <a.heilemann@gmail.com>
2020-09-15 16:04:01 +0200

    New vote button and user login/logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component  } from 'react';
-// import './App.css';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import './App.css';
 
 class App extends Component {
   state = {
@@ -36,19 +38,37 @@ class App extends Component {
     // .then(json => console.log(json))
   }
 
+  // loginLogout = (isLoggedIn) {
+  //   {isLoggedIn ? this.logInOut='logout' : 'login'}
+  //   <a href="http://127.0.0.1:8000/accounts/login/">{this.logInOut}</a>
+  //   <a href="http://127.0.0.1:8000/accounts/logout">{this.logInOut}</a>
+  // }
+
   render() {
+    const isLoggedIn = this.state.currentuser.name;
+    const logInOut = "login";
+    var loginButton;
+
     return (
       <div className="App">
         <header className="App-header">
+          <div>
+            {this.state.currentuser.name} {isLoggedIn 
+            ? <a href="http://127.0.0.1:8000/accounts/logout/">logout</a> 
+            : <a href="http://127.0.0.1:8000/accounts/login/">login</a>}
+          </div>
           {this.state.reviews.map(review => (
             <div key={review.id}>
-              <h1><a href={review.url}>{review.title}</a></h1>
-              by: <a href={review.author_url}>{review.author}</a> - 
-              Votes: {review.vote_score} 
-              <button onClick={() => this.handleClick(review, this.vote='up')}>Upvote!</button>
-              <button onClick={() => this.handleClick(review, this.vote='down')}>Downvote!</button>
-              
-              <p />{review.description}
+              <h1>
+                <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
+                  <Button variant="contained" onClick={() => this.handleClick(review, this.vote='down')}>-</Button>
+                  <Button disabled variant="text" color="default">{review.vote_score}</Button>
+                  <Button variant="contained" onClick={() => this.handleClick(review, this.vote='up')}>+</Button>
+                </ButtonGroup>
+                <a href={review.url}>{review.title}</a>
+              </h1>
+              {review.description}
+              <br /> by: <a href={review.author_url}>{review.author}</a>
             </div>
           ))}
         </header>
